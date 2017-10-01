71716abd13f2a9b170ed51580145298523617337
Yusuke Ikezaki <iappley08@gmail.com>
2017-10-01 23:02:22 +0900

    deleted lib/*Layer.java

--- lib/PoolingLayer.java
+++
@@ -1,9 +0,0 @@
-public class PoolingLayer extends Layer{
-    private Pair image_shape;
-    private Pair pooling_shape;
-
-    public PoolingLayer(Pair image_shape, Pair pooling_shape){
-	this.image_shape = image_shape;
-	this.pooling_shape = pooling_shape;
-    }
-}
